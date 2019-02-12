ba8ac12a72e7dcef9b89558acb5068410152b717
José Nogueira <josej@bndes.gov.br>
2019-02-12 16:49:08 -0200

    merge2

--- TRUW001A_010_init.js
+++ TRUW001A_010_init.js
@@ -2,27 +2,10 @@
 /* INIT                                                                                               */
 /******************************************************************************************************/
 
-var saptb_config = require('./TRUW001A_000_config.js');
+var saptb_config = require( './TRUW001A_000_config.js' );
 
 saptb_config.inicioLibVar(__filename)
 
-<<<<<<< HEAD
-    logger.info("Configuration:")
-    logger.info("---------------------------------------------------------------------------------")
-    logger.info("arqProjectID     = " + config.arqProjectID)
-    logger.info("arqTBitem        = " + config.arqTBitem)
-    logger.info("arqToken         = " + config.arqToken)
-    logger.info("arqSAP           = " + config.arqSAP)
-    logger.info("MOCK             = " + config.MOCK)
-    logger.info("logLevel         = " + config.logLevel)
-    logger.info("intervaloDias    = " + config.intervaloDias)
-    logger.info("tbNomeProjeto    = " + config.tb_nome_projeto)
-    logger.info("urlbasesap       = " + config.urlbasesap)
-    logger.info("urlbasetb        = " + config.urlbasetb)
-    logger.info("urlSapUser       = " + config.url_sap_user)
-    logger.info("urlSapPass       = " + "******************")
-    logger.info("---------------------------------------------------------------------------------")    
-=======
 logger.info("Configuration:")
 logger.info("---------------------------------------------------------------------------------")
 logger.info("arqProjectID     = " + config.arqProjectID)
@@ -37,7 +20,6 @@ logger.info("urlbasetb        = " + config.urlbasetb)
 logger.info("urlSapUser       = " + config.urlSapUser)
 logger.info("urlSapPass       = " + "******************")
 logger.info("---------------------------------------------------------------------------------")
->>>>>>> 88e58ae4299a82e807137cdcd667e94719bacb82
 
 
 logger.info( "Erase all files from previous run ... ")
